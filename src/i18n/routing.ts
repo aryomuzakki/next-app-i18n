62d7796f63f1775c38ad291fe6cf1c5309f7939a
Muhammad Aryo Muzakki <aryo.muzakki@gmail.com>
2026-03-05 04:39:27 +0700

    adjust cookies and specify other prefix options

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -7,8 +7,14 @@ export const routing = defineRouting({
   // Used when no locale matches
   defaultLocale: "en",
 
+  // 'never'     → all locale uses "/" without prefix
   // 'as-needed' → default locale uses "/" without prefix, others use "/id", "/ja"
   // 'always'    → all locales use prefix: "/en", "/id", "/ja"
   // Change this value to switch between strategies
   localePrefix: "as-needed",
+
+  localeCookie: {
+    name: "next_app_i18n_locale",
+    maxAge: 60 * 60 * 24 * 365, // one year
+  },
 });
